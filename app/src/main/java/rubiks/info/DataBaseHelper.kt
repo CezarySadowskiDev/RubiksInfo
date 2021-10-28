69adb4646dd719e4dd43b7a479a24f8aefd23ec6
battleczaro@gmail.com <Zohanziom16>
2021-10-29 01:19:30 +0200

    Dodanie całego ekranu statystyk

--- app/src/main/java/rubiks/info/DataBaseHelper.kt
+++ app/src/main/java/rubiks/info/DataBaseHelper.kt
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
 
 // Opis tabeli
-object TableInfo: BaseColumns {
+object TableInfo : BaseColumns {
     const val TABLE_NAME = "Times"
     const val TABLE_COLUMN_TIME = "time"
 }
@@ -21,7 +21,7 @@ object BasicCommands {
     const val SQL_DELETE_TABLE = "DROP TABLE IF EXISTS ${TableInfo.TABLE_NAME}"
 }
 
-class DataBaseHelper(context: Context): SQLiteOpenHelper(context, TableInfo.TABLE_NAME, null, 1) {
+class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, TableInfo.TABLE_NAME, null, 1) {
     override fun onCreate(p0: SQLiteDatabase?) {
         p0?.execSQL(BasicCommands.SQL_CREATE_TABLE)
     }
@@ -31,4 +31,90 @@ class DataBaseHelper(context: Context): SQLiteOpenHelper(context, TableInfo.TABL
         onCreate(p0)
     }
 
+    fun getData(dataType: String): String {
+        val db = this.readableDatabase
+
+        when (dataType) {
+            "solvesCount" -> {
+                return executeQuery(db, "SELECT count(*) FROM ${TableInfo.TABLE_NAME}")
+            }
+            "meanTime" -> {
+                return formatTime(
+                    executeQuery(
+                        db,
+                        "SELECT avg(${TableInfo.TABLE_COLUMN_TIME}) " +
+                                "FROM ${TableInfo.TABLE_NAME}"
+                    )
+                )
+            }
+            "meanTimeLastTenSolves" -> {
+                return formatTime(
+                    executeQuery(
+                        db,
+                        "SELECT avg(${TableInfo.TABLE_COLUMN_TIME}) " +
+                                "FROM (SELECT ${TableInfo.TABLE_COLUMN_TIME} " +
+                                "FROM ${TableInfo.TABLE_NAME} " +
+                                "ORDER BY _id DESC LIMIT 10)"
+                    )
+                )
+            }
+            "bestTime" -> {
+                return formatTime(
+                    executeQuery(
+                        db,
+                        "SELECT ${TableInfo.TABLE_COLUMN_TIME} " +
+                                "FROM ${TableInfo.TABLE_NAME} " +
+                                "ORDER BY ${TableInfo.TABLE_COLUMN_TIME} ASC LIMIT 1"
+                    )
+                )
+            }
+            "worstTime" -> {
+                return formatTime(
+                    executeQuery(
+                        db,
+                        "SELECT ${TableInfo.TABLE_COLUMN_TIME} " +
+                                "FROM ${TableInfo.TABLE_NAME} " +
+                                "ORDER BY ${TableInfo.TABLE_COLUMN_TIME} DESC LIMIT 1"
+                    )
+                )
+            }
+            "timeSpendOnSolving" -> {
+                return formatTime(
+                    executeQuery(
+                        db,
+                        "SELECT sum(${TableInfo.TABLE_COLUMN_TIME}) FROM ${TableInfo.TABLE_NAME}"
+                    )
+                )
+            }
+        }
+
+        return "0"
+    }
+
+    private fun executeQuery(dataBase: SQLiteDatabase, query: String): String {
+        val result = dataBase.rawQuery(query, null)
+
+        if (result.moveToFirst()) {
+            return result.getString(0)
+        }
+
+        result.close()
+
+        return "0"
+    }
+
+    private fun formatTime(value: String): String {
+
+        var properValue = value.toFloat().toInt()
+
+        val milliseconds = properValue % 1000
+        properValue /= 1000
+
+        val seconds = properValue % 60
+        properValue /= 60
+
+        return String.format("%02d", properValue) + ":" +
+                String.format("%02d", seconds) + ":" +
+                String.format("%03d", milliseconds)
+    }
 }
--- app/src/main/java/rubiks/info/StatisticsActivity.kt
+++ app/src/main/java/rubiks/info/StatisticsActivity.kt
@@ -1,9 +1,19 @@
 package rubiks.info
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TableLayout
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.marginTop
 
 class StatisticsActivity : AppCompatActivity() {
 
@@ -20,9 +30,49 @@ class StatisticsActivity : AppCompatActivity() {
         statisticsData.add(resources.getString(R.string.worst_time))
         statisticsData.add(resources.getString(R.string.time_spend_on_solving))
 
-        Toast.makeText(this, statisticsData[0], Toast.LENGTH_SHORT).show()
+        val db = DataBaseHelper(this)
 
-        val solvesCount = findViewById<TextView>(R.id.solvesCountData)
+        if (databaseExists(this, TableInfo.TABLE_NAME)) {
+
+            val solvesCountData = findViewById<TextView>(R.id.solvesCountData)
+            solvesCountData.text = db.getData("solvesCount")
+
+            val meanTimeData = findViewById<TextView>(R.id.meanTimeData)
+            meanTimeData.text = db.getData("meanTime")
+
+            val meanTimeLastTenSolvesData = findViewById<TextView>(R.id.meanTimeLastTenSolvesData)
+            meanTimeLastTenSolvesData.text = db.getData("meanTimeLastTenSolves")
+
+            val bestTimeData = findViewById<TextView>(R.id.bestTimeData)
+            bestTimeData.text = db.getData("bestTime")
+
+            val worstTimeData = findViewById<TextView>(R.id.worstTimeData)
+            worstTimeData.text = db.getData("worstTime")
+
+            val timeSpendOnSolvingData = findViewById<TextView>(R.id.timeSpendOnSolvingData)
+            timeSpendOnSolvingData.text = db.getData("timeSpendOnSolving")
+
+        } else {
+
+            val table = findViewById<TableLayout>(R.id.statisticsTable)
+            table.visibility = View.GONE
+
+            val textViewInfo = TextView(this)
+            textViewInfo.text = resources.getString(R.string.no_database_info)
+            textViewInfo.textSize = 20f
+            textViewInfo.setTextColor(Color.WHITE)
+            textViewInfo.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            textViewInfo.gravity = Gravity.CENTER
+
+            val statisticsMainLayout = findViewById<LinearLayout>(R.id.statisticsMainLayout)
+            statisticsMainLayout.addView(textViewInfo)
+        }
+    }
+
+    private fun databaseExists(context: Context, databaseName: String): Boolean {
+
+        val dbFile = context.getDatabasePath(databaseName)
+        return dbFile.exists()
 
     }
 }
